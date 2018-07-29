0588c6ffacf9263bf9c892c0aa4f649d5d8ee233
Brandon Ward <Wards@brandonsmacbook.attlocal.net>
2018-07-29 13:13:43 -0700

    add descriptions and GH button

--- js/cascadients.js
+++ js/cascadients.js
@@ -438,3 +438,13 @@ colorSelect.forEach((color) => {
     });
   });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
